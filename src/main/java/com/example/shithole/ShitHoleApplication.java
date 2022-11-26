0cfb9a28d2dbacdd1530ff85e47f8a0093bba19a
xbulat91 <xbulat91@gmail.com>
2022-11-26 21:08:21 +0300

    init commit

---
+++ src/main/java/com/example/shithole/ShitHoleApplication.java
@@ -0,0 +1,13 @@
+package com.example.shithole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShitHoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShitHoleApplication.class, args);
+    }
+
+}
